dd7fab6de0ea9b529222fce46663fcd90376a15b
NezzNH <nezziehowl@gmail.com>
2026-03-05 19:09:30 +0100

    rewriting pieceParser

--- ChessInCpp/pieceParser.cpp
+++ ChessInCpp/pieceParser.cpp
@@ -30,43 +30,20 @@ Piece* PieceParser::parsePieces(int* outputSize, std::string filePath) {
 
     std::ifstream pieceDefsFile(filePath);
 
-    std::string buffer, prevLines, lastMovePair;
+    std::string buffer, prevLines = "", lastMovePair;
 
     while(getline(pieceDefsFile, buffer)) {
         if (buffer == "PIECEDEF") {
-            if (prevLines == "" || prevLines == "PIECEDEF") continue;
-            else {
-                if (buffer[0] == 'D' || buffer[0] == 'R') {
-                    bool startedPair = false;
-                    for (int i = 2; i < buffer.length(); ++i) {
-                        if (buffer[i] == '[') {
-                            startedPair = true;
-                        }
-                        else if (buffer[i] == ']' && startedPair) {
-                            moveStructureSize++; startedPair = false;
-                        }
-                        else continue;
-                    }
-
-                    tempMovesArray = new coordPair[moveStructureSize];
-
-                    for (int i = 2; i < buffer.length(); ++i) {
-                        std::string half1, half2;
-                        int j = 0;
-                        if (buffer[i] == '[') lastMovePair.clear();
-                        else if (buffer[i] == ']') {
-                            while (lastMovePair[j] != ',') {
-                                half1 += lastMovePair[j]; ++j;
-                            }
-                            half2 = lastMovePair.substr(j+1, lastMovePair.length() - (j+1)); //not sure if substr wants length or positions
-                            
-                        }
-                        else lastMovePair += buffer[i];
-                    }
-                }
+            if (prevLines != "") {
+                
             }
+            else prevLines += buffer + " ";
         }
     }
 
+
+}
+
+MoveStructures compose_move_structure(std::string inputString) {
 
 }
